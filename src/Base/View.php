3b30f7f63f86099558323d51ef20b9b6988e882e
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2023-09-07 13:43:37 +0300

    Add Base View class with render method

--- src/Base/View.php
+++ src/Base/View.php
@@ -4,5 +4,17 @@ namespace MVC\Framework\Base;
 
 class View
 {
-    
+    public static function render(string $view, array $params = [])
+    {
+        
+        $file = VIEWS_DIR . $view .  (!str_ends_with($view, '.php') ? '.php' : '');
+        
+        extract($params, EXTR_SKIP);
+
+        if (is_readable($file)) {
+            require $file;            
+        } else {
+            die("$view does not exist");
+        }
+    }
 }
